c51db17a9a0c6a87415ef3e79f6755424cc5ce73
Quent-Repo <grrkekisbest@gmail.com>
2024-04-16 22:12:35 -0400

    Leet 26

--- 26.py
+++ 26.py
@@ -2,6 +2,6 @@ Start = 1
 
 for go in range(1, len(nums):
                 if nums[go] != nums[go-1]:
-                    nums{Start] = nums[R]
+                    nums{Start] = nums[go]
                     Start += 1
 return Start
